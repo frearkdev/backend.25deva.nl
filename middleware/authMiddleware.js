4810a44f0d163ee0834d7a7316f85450f915e4a1
Freark Bergstra <514721@student.firda.nl>
2025-12-04 09:39:41 +0100

    Added source files

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+function authRequired(req, res, next) {
+    const header = req.headers.authorization;
+
+    if (!header) {
+        return res.status(401).json({ error: "No token provided" })
+    }
+
+    const token = header.startsWith('Bearer ')
+        ? header.split(' ')[1]
+        : header;
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+        next();
+    } catch (err) {
+        return res.status(401).json({ error: 'Invalid token' })
+    }
+}
+
+module.exports = authRequired;
